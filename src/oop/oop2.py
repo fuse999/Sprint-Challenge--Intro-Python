251b4e19d49165480dc1acf929fb423d41c5c532
riley pence <rileypence4@gmail.com>
2020-03-13 09:23:25 -0700

    fixed oop2

--- src/oop/oop2.py
+++ src/oop/oop2.py
@@ -9,9 +9,9 @@ class GroundVehicle():
 
     def drive(self):
         if self.num_wheels == 4:
-            print("vroooom")
+            return "vroooom"
         elif self.num_wheels == 2:
-            print("BRAAAP!!")
+            return "BRAAAP!!"
 
 
 # Subclass Motorcycle from GroundVehicle.
@@ -36,4 +36,4 @@ vehicles = [
 # Go through the vehicles list and print the result of calling drive() on each.
 
 for vehicle in vehicles:
-    vehicle.drive()
+    print(vehicle.drive())
